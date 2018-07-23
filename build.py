a9e4969506dab4367845bee84ffecd6267db04ee
Dmitry Bely <d.bely@recognize.ru>
2018-07-23 10:44:58 +0300

    Sync with cmake-package-tools API changes

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="dbely", args="--build missing")
+    builder = ConanMultiPackager(build_policy="missing")
     builder.visual_runtimes = ["MD", "MDd"]
     builder.add_common_builds(shared_option_name="rabbitmq-c:shared", pure_c=False)
     builder.run()
